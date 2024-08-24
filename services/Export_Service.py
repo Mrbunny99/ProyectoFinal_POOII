f0bc1707395e6d45f825be3bd4367d6458c7fe4f
Anthony99 <anthonyjosue2000@gmail.com>
2024-08-24 15:36:39 -0500

    Update Export_Service.py

--- services/Export_Service.py
+++ services/Export_Service.py
@@ -3,12 +3,9 @@ from sqlalchemy.orm import Session
 from accessData.entities.models import Estudiante, Publicacion
 
 def export_table(db: Session, table_name: str):
-    #Aqui se obtiene la fecha actual
     date_str = datetime.datetime.now().strftime("%d-%m-%Y")
-    #Crea el nombre del archivo
     file_name = f'{table_name}-{date_str}.txt'
     
-    #Consulta todos los registros de la tabla especifica
     if table_name == 'estudiante':
         records = db.query(Estudiante).all()
     elif table_name == 'publicacion':
@@ -16,7 +13,6 @@ def export_table(db: Session, table_name: str):
     else:
         raise ValueError("Tabla no soportada")
 
-    #Aqui escribe los registros en el archivo
     with open(file_name, 'w') as file:
         for record in records:
             file.write(f'{record}\n')
